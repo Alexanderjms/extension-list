1f8c04a3374546165d4b45fe49168aa98d8df40a
Alexanderjms <jasson.suazo@unah.hn>
2025-05-04 12:30:42 -0600

    Background color

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+            <h1 className="text-2xl">SAOIDHKJASDGSAGDASGD</h1>
+        </header>
+    );
+}
